3552b112801beed83a7b3a009d6d1744ad61ac43
Chris Junker <elevatemedfit@gmail.com>
2019-07-05 15:30:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,13 @@
 class School
   def initialize(name)
     @name = name
-    roster = Hash.new
   end
-
+  def roster=(roster)
+@roster = Hash.new
+end
+def roster
+  @roster
+end
   def name
     @name
   end
