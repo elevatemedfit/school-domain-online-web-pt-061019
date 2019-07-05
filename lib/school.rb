79e1e801c0e6872ecfbcdd0ab469f85ca4c4c480
Chris Junker <elevatemedfit@gmail.com>
2019-07-05 15:14:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,19 +12,4 @@ class School
   end
 
 
-  def student
-    @student
-  end
-
-  def add_student(name,grade)
-    if roster[grade]==grade
-      roster[grade] << name
-    else
-    roster[grade]=[]
-    roster[grade]<<name
-    end
-    roster
-  end
-
-
 end# code here!
