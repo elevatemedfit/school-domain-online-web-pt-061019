ad8cf471778ccd36fbd48bfee0b57e211f0d6c38
Chris Junker <elevatemedfit@gmail.com>
2019-07-05 16:30:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,12 @@
 class School
+  attr_accessor :roster
   def initialize(name)
     @name = name
     @roster = {}
   end
-  def roster
-    @roster
-  end
+  # def roster
+  #   @roster
+  # end
 
 
 end# code here!
