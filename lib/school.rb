eb8b6a5afa98d295c816c08000a371354637e9ee
Chris Junker <elevatemedfit@gmail.com>
2019-07-05 15:02:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+  def initialize(school)
+    @school = school
+  end
+
+  def school
+    @school
+  end
+
+  def initialize(roster)
+    @roster = Hash.new
+  end
+
+  def roster
+    @roster
+  end
+
+  def student
+    @student
+  end
+
+  def add_student(name,grade)
+    if roster[grade]==grade
+      roster[grade] << name
+    else
+    roster[grade]=[]
+    roster[grade]<<name
+    end
+    roster
+  end
+
+
+end# code here!
